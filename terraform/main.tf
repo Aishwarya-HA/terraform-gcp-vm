39fa6c5f32b0eb1c773626a539d5e6d4220a40be
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-04-22 19:56:41 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,64 @@
+resource "google_compute_network" "vpc" {
+  name                    = "custom-vpc"
+  auto_create_subnetworks = false
+}
 
+resource "google_compute_subnetwork" "subnet" {
+  name          = "custom-subnet"
+  ip_cidr_range = "10.0.0.0/24"
+  region        = var.region
+  network       = google_compute_network.vpc.id
+}
+
+resource "google_compute_firewall" "allow_ssh" {
+  name    = "allow-ssh"
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = ["22"]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_firewall" "allow_app" {
+  name    = "allow-app"
+  network = google_compute_network.vpc.name
+
+  allow {
+    protocol = "tcp"
+    ports    = [var.app_port]
+  }
+
+  source_ranges = ["0.0.0.0/0"]
+}
+
+resource "google_compute_address" "public_ip" {
+  name   = "vm-public-ip"
+  region = var.region
+}
+
+resource "google_compute_instance" "vm" {
+  name         = "ubuntu-vm"
+  machine_type = "e2-medium"
+  zone         = var.zone
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+    }
+  }
+
+  network_interface {
+    subnetwork = google_compute_subnetwork.subnet.id
+
+    access_config {
+      nat_ip = google_compute_address.public_ip.address
+    }
+  }
+
+  metadata = {
+    ssh-keys = "${var.ssh_user}:${file(var.ssh_public_key_path)}"
+  }
+}
