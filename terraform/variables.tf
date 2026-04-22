a586e7771817f333c45c4a430113a4366eb68cbd
Aishwarya-HA <124653467+Aishwarya-HA@users.noreply.github.com>
2026-04-22 19:55:16 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,1 +1,29 @@
+variable "project_id" {
+  description = "GCP Project ID"
+  type        = string
+}
 
+variable "region" {
+  description = "GCP Region"
+  type        = string
+}
+
+variable "zone" {
+  description = "GCP Zone"
+  type        = string
+}
+
+variable "app_port" {
+  description = "Application port to allow"
+  type        = string
+}
+
+variable "ssh_user" {
+  description = "SSH username"
+  type        = string
+}
+
+variable "ssh_public_key_path" {
+  description = "Path to SSH public key"
+  type        = string
+}
